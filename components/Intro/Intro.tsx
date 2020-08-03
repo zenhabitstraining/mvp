7bc3237894f8bccfca8f8df85174d62efbd0d792
Alex Price <hi@alexpriceonline.com>
2020-08-03 08:55:06 +0100

    Updated styles to feel more like zen-habits

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -5,7 +5,7 @@ import { Video } from "../Video";
 export const Intro = () => (
   <div className="wrapper">
     <h1>Welcome to the Mindfulness Protocol</h1>
-    <p className="intro bp3-text-large">
+    <p className="intro">
       The new mindfulness training and experiment by{" "}
       <strong>Leo Babauta</strong> of{" "}
       <a href="https://https://zenhabits.net/">Zen Habits</a>.
@@ -14,25 +14,25 @@ export const Intro = () => (
     <br />
     <br />
     <h2>We invite you to join us!</h2>
-    <p className="bp3-text-large">
-      We’re offering a daily training system to:
-      <ul>
-        <li>
-          Increase <strong>focus</strong>
-        </li>
-        <li>
-          Improve <strong>mindfulness</strong>
-        </li>
-        <li>
-          Cope with <strong>stress</strong>
-        </li>
-        <li>
-          Be better at <strong>habit creation</strong>
-        </li>
-        <li>
-          Be more <strong>compassionate</strong>
-        </li>
-      </ul>
+    <p>We’re offering a daily training system to:</p>
+    <ul>
+      <li>
+        Increase <strong>focus</strong>
+      </li>
+      <li>
+        Improve <strong>mindfulness</strong>
+      </li>
+      <li>
+        Cope with <strong>stress</strong>
+      </li>
+      <li>
+        Be better at <strong>habit creation</strong>
+      </li>
+      <li>
+        Be more <strong>compassionate</strong>
+      </li>
+    </ul>
+    <p>
       It will only take 10 minutes a day, and it’s{" "}
       <strong>completely free</strong>. You’ll also be helping to contribute to
       our experiment to create an evidence-based method to train the mind.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
 
@@ -7,19 +8,28 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 
 const MindfulnessApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <div className="page-wrapper">
-    <div className="content-wrapper">
-      <Provider session={pageProps.session}>
-        <Component {...pageProps} />
-      </Provider>
+  <>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+
+    <div className="page-wrapper">
+      <div className="content-wrapper">
+        <Provider session={pageProps.session}>
+          <Component {...pageProps} />
+        </Provider>
+      </div>
     </div>
 
     <style jsx>{`
       .page-wrapper {
-        background-color: #f5f8fa;
-        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23e1e8ed' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23bfccd6' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23dde5ea' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23bac8d2' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d9e2e8' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23b5c3ce' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23d6dfe5' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23b1bfca' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23d2dce3' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%23acbac6' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23ced9e0' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23a7b6c2' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
         background-attachment: fixed;
-        background-size: cover;
+        font-family: "Nunito Sans", -apple-system, BlinkMacSystemFont,
+          "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue",
+          sans-serif;
         min-height: 100vh;
         padding-bottom: 30px;
       }
@@ -28,11 +38,33 @@ const MindfulnessApp: React.FC<AppProps> = ({ Component, pageProps }) => (
         background-color: rgba(245, 248, 250, 0.6);
         border-radius: 5px;
         margin: 0 auto;
-        max-width: 650px;
+        max-width: 660px;
         padding: 20px;
       }
     `}</style>
-  </div>
+
+    <style jsx global>{`
+      h1 {
+        font-weight: 300;
+        font-size: 36px;
+      }
+
+      h2 {
+        font-weight: 300;
+        font-size: 27px;
+      }
+
+      p,
+      ul {
+        font-weight: 300;
+        font-size: 18px;
+      }
+
+      strong {
+        font-weight: 400;
+      }
+    `}</style>
+  </>
 );
 
 export default MindfulnessApp;
